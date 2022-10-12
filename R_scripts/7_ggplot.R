91f992f642e41098bbf8f2bb01ba4fc8396e7194
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-10-11 17:01:14 -0700

    plot

--- R_scripts/7_ggplot.R
+++ R_scripts/7_ggplot.R
@@ -1,4 +1,7 @@
+library(patchwork)
+library(ggpubr)
 
+names(ST1)
 names(ST1[[13]])
 df1 <- as.data.frame(ST1[[13]][[14]][[1]])
 head(df1)
@@ -23,7 +26,7 @@ head(df1)
 # A
 A <- ggplot(df1, aes(x=FD, y=predicted.value)) + 
   geom_point(size = 1) +
-  geom_errorbar(aes(ymin = lower.Confidence.limit, ymax = upper.Confidence.limit), width = 0.1, alpha = 0.6) + theme_classic(base_family = "Arial", base_size = 12)
+  geom_errorbar(aes(ymin = lower.Confidence.limit, ymax = upper.Confidence.limit), width = 0.1, alpha = 0.6) + theme_classic(base_family = "Arial", base_size = 12) + labs(y = "", x = "FD")
 
 # B
 df1 <- as.data.frame(ST1[[13]][[2]][[1]])
@@ -31,9 +34,9 @@ head(df1)
 B <- ggplot(df1, aes(x=loc, y=predicted.value)) + 
   geom_point(size = 1) +
   geom_errorbar(aes(ymin = lower.Confidence.limit, ymax = upper.Confidence.limit), width = 0.1, alpha = 0.6) + 
-  theme_classic(base_family = "Arial", base_size = 12)
+  theme_classic(base_family = "Arial", base_size = 12) + labs(y = "", x = "Loc")
 
-names((ST1[[13]]))
+
 # FD_loc
 # C
 df1 <- as.data.frame(ST1[[13]][[5]][[1]])
@@ -45,21 +48,12 @@ C <- ggplot(df1, aes(x=FD, y=predicted.value, group = loc)) +
   geom_point(size = 1, aes(shape=loc, color = loc), na.rm=TRUE, position=position_dodge(width=0.8)) +
   scale_shape_manual(values = c(15, 16, 18)) +
   theme_classic(base_family = "Arial", base_size = 12) +
-  theme(legend.position="none") + labs(y = "", x = "FD")
+  theme(legend.position="none") + labs(y = "", x = "FD by Loc")
 
 # FD loc env4 (year_month)
 
 df1 <- as.data.frame(ST1[[13]][[11]][[1]])
 head(df1)
-
-
-
-
-ggplot(df1, aes(x=FD, y=predicted.value)) + 
-  geom_point(size = 1, aes(shape=loc), na.rm=TRUE, position=position_dodge(width=0.5)) +
-  scale_shape_manual(values = c(2, 0, 1)) +
-  geom_errorbar(aes(ymin = lower.Confidence.limit, ymax = upper.Confidence.limit), width = 0.1, na.rm=TRUE, position=position_dodge(width=0.5)) +
-  theme_classic(base_family = "Arial", base_size = 12)
 
 # D
 
@@ -71,6 +65,16 @@ D <- ggplot(df1, aes(x=env4, y=predicted.value, group=loc)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 0.95, vjust = 0.2), legend.position="none") +
   facet_grid(. ~ FD, scales = "free_x", space = "free") + labs(y = "", x = "Harvest")
 
+A + B + C + D + plot_layout(ncol = 2)
+
+ggarrange(A, B, widths = c(1, 0.5), ncol = 2, nrow = 1, align = "h")
+
+ggarrange(ggarrange(A, B, widths = c(1, 0.5), ncol = 2, nrow = 1), C, ncol = 2, nrow = 1)
+
+E <- ggarrange(ggarrange(ggarrange(A, B, widths = c(1, 0.5), ncol = 2), C, ncol = 2, nrow = 1), D, ncol = 1, nrow = 2, heights = c(0.7, 1), align = "v")
+
+setwd("~/Documents/git/Dreger_2022/figs/")
+ggsave(filename = "Lignin.jpg", plot = E, width = 15, height = 6)
 
 
 
